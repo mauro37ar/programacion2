39e47efb0a69f007017f24f8351e789e8e4d2ff5
mauro37ar <mauro31ar@gmail.com>
2017-05-27 16:43:58 -0300

    ejercicios unidad1

---
+++ practica_3/ejercicio_2.py
@@ -0,0 +1,14 @@
+c#!/usr/bin/python
+
+p1 =raw_input ("ingrese su nombre: ")
+
+P2 =raw_input ("ingrese su apellido: ")
+
+p3 =input ("ingrese su edad: ")
+
+p4 =input ("ingrese su dni: ")
+
+cuenta = ["p1", "p2", "p3", "p4"]
+ 
+contraseña = cuenta ("p1"*"p4")
+
