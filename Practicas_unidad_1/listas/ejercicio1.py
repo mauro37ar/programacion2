83ba858c9feb3f2d82ac59e2b3a704f368974d29
mauro37ar <mauro31ar@gmail.com>
2017-06-02 13:28:23 -0300

    practica listas

---
+++ Practicas_unidad_1/listas/ejercicio1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+Personas  ={}
+p1=raw_input ("ingrese su nombre: ")
+p2=raw_input ("ingrese su apellido: ")
+p3=input ("ingrese su dni: ")
+p4=input("anio de nacimiento: ")
+p5=raw_input ("ingrese su lugar de nacimiento: ")
+personas=[p1, p2, p3, p4, p5, 0]
+ 
+
+print (personas)
