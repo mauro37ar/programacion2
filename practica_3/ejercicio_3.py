3fe2acc3a76e5cb36a273a7007216205f11146f9
mauro37ar <mauro31ar@gmail.com>
2017-06-29 15:39:25 -0300

    subidas

---
+++ practica_3/ejercicio_3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+p1 =raw_input ("ingrese su nombre: ")
+
+p2 =raw_input ("ingrese su apellido: ")
+
+p3 =input ("ingrese su edad: ")
+
+p4=input ("ingrese su dni: ")
+
+p5=len(p1+p2)*p4
+
+cuenta=[p1,p2,p3,p4,p5]
+
+print (cuenta)
---
+++ practica_3/ejercicio_3_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+contrasenia = 200
+
+cuenta = 0
+
+while cuenta < contrasenia:
+
+	print "el numero es:", cuenta
+
+	cuenta = cuenta + 1
+
