690a5f6eb6100467b510302bb936a4a6bb82a661
mauro37ar <mauro31ar@gmail.com>
2017-08-24 13:11:12 -0300

    ejercicio 7

---
+++ practica_4/ejercicio 7.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+leia=raw_input("ingrese palabra: ")
+
+def inversa(L):
+
+    nue = ""
+
+    if len(l)==1:
+
+        return l
+
+    else:
+
+        nue = 1 + nue
+
+        return nue
+
+print inversa(leia)
